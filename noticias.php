d8ed1fed212dfb9b1ea6fb4bf761b9f0d276011d
Cesar Ibarra <ces2294@gmail.com>
2016-05-15 21:31:02 -0600

    Preparacion para uso dinamico de noticias con PHP

--- noticias.php
+++ noticias.php
@@ -33,67 +33,19 @@
 	</div>
 	<div id="contents">
 		<div class="main">
-			<h1>News</h1>
+			<h1>Noticias</h1>
 			<ul class="news">
-				<li>
-					<div class="date">
-						<p>
-							<span>03</span>
-							2023
-						</p>
-					</div>
-					<h2>Updates: More Features Released <span>Brian Ferry</span></h2>
-					<p>
-						You can replace all this text with your own text. Want an easier solution for a Free Website? Head straight to Wix and immediately start customizing your website! Wix is an online website builder with a simple drag &amp; drop interface, meaning you do the work online and instantly publish to the web. All Wix templates are fully customizable and free to use. Just pick one you like, click Edit, and enter the online editor. <span><a href="post.html" class="more">Read More</a></span>
-					</p>
-				</li>
-				<li>
-					<div class="date">
-						<p>
-							<span>03</span>
-							2023
-						</p>
-					</div>
-					<h2>Updates: More Features Released <span>Brian Ferry</span></h2>
-					<p>
-						You can replace all this text with your own text. Want an easier solution for a Free Website? Head straight to Wix and immediately start customizing your website! Wix is an online website builder with a simple drag &amp; drop interface, meaning you do the work online and instantly publish to the web. All Wix templates are fully customizable and free to use. Just pick one you like, click Edit, and enter the online editor. <span><a href="post.html" class="more">Read More</a></span>
-					</p>
-				</li>
-				<li>
-					<div class="date">
-						<p>
-							<span>03</span>
-							2023
-						</p>
-					</div>
-					<h2>Updates: More Features Released <span>Brian Ferry</span></h2>
-					<p>
-						You can replace all this text with your own text. Want an easier solution for a Free Website? Head straight to Wix and immediately start customizing your website! Wix is an online website builder with a simple drag &amp; drop interface, meaning you do the work online and instantly publish to the web. All Wix templates are fully customizable and free to use. Just pick one you like, click Edit, and enter the online editor. <span><a href="post.html" class="more">Read More</a></span>
-					</p>
-				</li>
+				<?php
+					//Insertar aqui codigo para agregado dinamico de noticias
+				?>
 			</ul>
 		</div>
 		<div class="sidebar">
-			<h1>Popular Posts</h1>
+			<h1>Noticias Populares</h1>
 			<ul class="posts">
-				<li>
-					<h4 class="title"><a href="post.html">Making It Work</a></h4>
-					<p>
-						I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you.
-					</p>
-				</li>
-				<li>
-					<h4 class="title"><a href="post.html">Designs and Concepts</a></h4>
-					<p>
-						I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you.
-					</p>
-				</li>
-				<li>
-					<h4 class="title"><a href="post.html">Getting It Done!</a></h4>
-					<p>
-						I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you.
-					</p>
-				</li>
+				<?php
+					//Codigo PHP
+				?>
 			</ul>
 		</div>
 	</div>
